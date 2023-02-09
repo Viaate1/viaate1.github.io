6da56db4d352aa6a8330ac9b8c264fc8a3c97401
Viaate1 <87151267+Viaate1@users.noreply.github.com>
2023-02-09 12:33:52 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const debtorSelect = document.getElementById("debtor-select");
+const debtorRating = document.getElementById("debtor-rating");
+const addDebtorForm = document.getElementById("add-debtor-form");
+const addRatingForm = document.getElementById("add-rating-form");
+const averageRating = document
